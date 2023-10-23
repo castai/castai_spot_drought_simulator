09f490a18a097d09314c350218614e851300eea5
Curtiss Porter <curtiss.porter82@gmail.com>
2023-10-23 16:29:31 -0400

    Refactored static instance family lists

--- src/castai_spot_drought.py
+++ src/castai_spot_drought.py
@@ -2,210 +2,25 @@ import os
 import requests
 
 castai_api_key: str = os.getenv("CASTAI_API_KEY")
-
 organization_id: str = "CASTAI_ORGANIZATION_ID"
-
 cluster_id: str = "CASTAI_CLUSTER_ID"
-
-aks_instance_families: list = [
-    "standard_DADSv5",
-    "standard_DASv4",
-    "standard_DASv5",
-    "standard_DAv4",
-    "standard_DCADSv5_CC",
-    "standard_DCASv5_CC",
-    "standard_DCDSv3",
-    "standard_DCS",
-    "standard_DCSv2",
-    "standard_DCSv3",
-    "standard_DCv2",
-    "standard_DDSv4",
-    "standard_DDSv5",
-    "standard_DDv4",
-    "standard_DDv5",
-    "standard_DLDSv5",
-    "standard_EBSv5",
-    "standard_ECADSv5_CC",
-    "standard_ECASv5_CC",
-    "standard_EDSv4",
-    "standard_EDSv5",
-    "standard_EDv4",
-    "standard_EDv5",
-    "standard_EIADSv5",
-    "standard_EIASv5",
-    "standard_EIBDSv5",
-    "standard_EIBSv5",
-    "standard_EIDSv4",
-    "standard_EIDSv5",
-    "standard_EIDv5",
-    "standard_EISv3",
-    "standard_EISv4",
-    "standard_EISv5",
-    "standard_EIv5",
-    "standard_ESv3",
-    "standard_ESv4",
-    "standard_ESv5",
-    "standard_Ev4",
-    "standard_Ev5",
-    "standard_FS",
-    "standard_FSv2",
-    "standard_FXDS",
-    "standard_HBRS",
-    "standard_HBRSv2",
-    "standard_HBRSv3",
-    "standard_HBRSv4",
-    "standard_HCRS",
-    "standard_HXRS",
-    "standard_LASv3",
-    "standard_LSv2",
-    "standard_LSv3",
-    "standard_M",
-    "standard_MDMSv2",
-    "standard_MDSv2",
-    "standard_MIDMSv2",
-    "standard_MIDSv2",
-    "standard_MISv2",
-    "standard_MLS",
-    "standard_MS",
-    "standard_MSv2",
-    "standard_MSv2_8",
-    "standard_MTS",
-    "standard_NCADSv4_A100",
-    "standard_NCASv3_T4",
-    "standard_NCRSv2",
-    "standard_NCRSv3",
-    "standard_NCSv2",
-    "standard_NCSv3",
-    "standard_NDAMSRv4_A100",
-    "standard_NDASRv4",
-    "standard_NDRS",
-    "standard_NDRSv2",
-    "standard_NDS",
-    "standard_NPS",
-    "standard_NVADMSv5_A10",
-    "standard_NVADSv5_A10",
-    "standard_NVASv4",
-    "standard_NVSv3",
-    "standard_PBS",
-]
-
-eks_instance_families: list = [
-    "c5",
-    "c5a",
-    "c5ad",
-    "c5d",
-    "c5n",
-    "c6a",
-    "c6g",
-    "c6gd",
-    "c6gn",
-    "c6i",
-    "c6id",
-    "c6in",
-    "c7a",
-    "c7g",
-    "c7gd",
-    "c7gn",
-    "c7i",
-    "d2",
-    "d3",
-    "d3en",
-    "dl1",
-    "f1",
-    "g3",
-    "g3s",
-    "g4ad",
-    "g4dn",
-    "g5",
-    "g5g",
-    "h1",
-    "i3",
-    "i3en",
-    "i4g",
-    "i4i",
-    "im4gn",
-    "inf1",
-    "inf2",
-    "is4gen",
-    "m5",
-    "m5a",
-    "m5ad",
-    "m5d",
-    "m5dn",
-    "m5n",
-    "m5zn",
-    "m6a",
-    "m6g",
-    "m6ad",
-    "m6gd",
-    "m6i",
-    "m6id",
-    "m6idn",
-    "m6in",
-    "m7a",
-    "m7g",
-    "m7gd",
-    "m7i",
-    "m7i-flex",
-    "p2",
-    "p3",
-    "p4d",
-    "p5",
-    "r5",
-    "r5a",
-    "r5ad",
-    "r5b",
-    "r5b",
-    "r5d",
-    "r5dn",
-    "r5n",
-    "r6a",
-    "r6g",
-    "r6gd",
-    "r6i",
-    "r6id",
-    "r6idn",
-    "r7a",
-    "r7g",
-    "r7gd",
-    "r7i",
-    "r7iz",
-    "trn1",
-    "u-12tb1",
-    "u-18tb1",
-    "u-3tb1",
-    "u-6tb1",
-    "u-9tb1",
-    "vt1",
-    "x1",
-    "x1e",
-    "x2gd",
-    "x2idn",
-    "x2iedn",
-    "x2izn",
-    "z1d",
-]
-
-gke_instance_families: list = [
-    "a2",
-    "c2",
-    "c2d",
-    "c3",
-    "e2",
-    "g2",
-    "m1",
-    "n1",
-    "n2",
-    "n2d",
-    "t2a",
-    "t2d",
-]
-
 blacklist_headers: dict = {
     "accept": "application/json",
     "content-type": "application/json",
     "X-API-Key": f"{castai_api_key}",
 }
+
+
+def extract_instance_families() -> list:
+    filter_instance_types_url: str = f"https://api.cast.ai/v1/kubernetes/clusters/{cluster_id}/filter-instance-types"
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "X-API-Key": f"{castai_api_key}"
+    }
+    response = requests.post(filter_instance_types_url, headers=headers)
+    available_instance_types = response.json()
+    return list(set([instance["family"] for instance in available_instance_types["availableInstanceTypes"]]))
 
 
 def add_to_blacklist(instance_families: list) -> list:
@@ -248,6 +63,7 @@ def remove_from_blacklist(instance_families: list) -> list:
 
 
 def interactive_mode():
+    families = extract_instance_families()
     while True:
         print("Choose an option:")
         print("1: Add to blacklist")
@@ -257,36 +73,19 @@ def interactive_mode():
 
         choice = input("Enter your choice (1/2/3/4): ")
 
-        if choice in ["1", "3"]:
-            print("Select instance family list:")
-            print("a: AKS")
-            print("b: EKS")
-            print("c: GKE")
-            family_choice = input("Enter your choice (a/b/c): ")
-
-            if family_choice == "a":
-                families = aks_instance_families
-            elif family_choice == "b":
-                families = eks_instance_families
-            elif family_choice == "c":
-                families = gke_instance_families
-            else:
-                print("Invalid choice for instance family. Try again.")
-                continue
-
-            if choice == "1":
-                add_responses = add_to_blacklist(families)
-                print(add_responses)
-            elif choice == "3":
-                remove_responses = remove_from_blacklist(families)
-                print(remove_responses)
+        if choice == "1":
+            add_responses = add_to_blacklist(families)
+            print(add_responses)
         elif choice == "2":
             blacklist = get_blacklist()
             print(blacklist)
+        elif choice == "3":
+            remove_responses = remove_from_blacklist(families)
+            print(remove_responses)
         elif choice == "4":
             break
         else:
-            print("Invalid main menu choice. Try again.")
+            print("Invalid choice. Try again.")
 
 
 if __name__ == "__main__":
